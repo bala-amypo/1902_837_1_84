2cdd6f86051d62ec3a6ca12761546ab04225a93d
bala-amypo <bala@amypo.in>
2025-12-21 07:30:54 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/UserServiceImpl.java
@@ -5,7 +5,7 @@ import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.security.JwtTokenProvider;
 import com.example.demo.service.UserService;
-
+@Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
