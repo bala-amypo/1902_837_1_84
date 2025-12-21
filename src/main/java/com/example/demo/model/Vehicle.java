3c17ef02f53a0bb044a2de3c2565956e630d9b2f
bala-amypo <bala@amypo.in>
2025-12-21 12:48:26 +0000

    User commit

--- src/main/java/com/example/demo/model/Vehicle.java
+++ src/main/java/com/example/demo/model/Vehicle.java
@@ -1,52 +1,23 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Table(name = "vehicles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Vehicle {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
     private String vin;
-
-    private String ownerName;
-    private Boolean active = true;
-
-    public Vehicle() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getVin() {
-        return vin;
-    }
-
-    public String getOwnerName() {
-        return ownerName;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setVin(String vin) {
-        this.vin = vin;
-    }
-
-    public void setOwnerName(String ownerName) {
-        this.ownerName = ownerName;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
+    private String make;
+    private String model;
+    private Integer year;
+    private Long ownerId;
 }
