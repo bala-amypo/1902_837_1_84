5eb50cdbf5a9ccf441dac2d2606c162af73864df
bala-amypo <bala@amypo.in>
2025-12-21 13:13:54 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleHealthServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleHealthServlet.java
@@ -1,20 +1,20 @@
-package com.example.demo.serviet;
+// package com.example.demo.serviet;
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.annotation.WebServlet;
+// import jakarta.servlet.http.HttpServlet;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
+// import jakarta.servlet.annotation.WebServlet;
 
-import java.io.IOException;
+// import java.io.IOException;
 
-@WebServlet(urlPatterns = "/health")
-public class SimpleHealthServlet extends HttpServlet {
+// @WebServlet(urlPatterns = "/health")
+// public class SimpleHealthServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
+//     @Override
+//     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+//             throws IOException {
 
-        resp.setStatus(HttpServletResponse.SC_OK);
-        resp.getWriter().write("OK");
-    }
-}
+//         resp.setStatus(HttpServletResponse.SC_OK);
+//         resp.getWriter().write("OK");
+//     }
+// }
